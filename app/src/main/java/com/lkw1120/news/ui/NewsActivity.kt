d14263a0d3c5f4c7e21cd2488482f8a24dec41b4
lkw1120 <lkwkang@gmail.com>
2020-03-31 17:15:20 +0900

    Feat : 뉴스 상세보기 구현

--- app/src/main/java/com/lkw1120/news/ui/NewsActivity.kt
+++ app/src/main/java/com/lkw1120/news/ui/NewsActivity.kt
@@ -1,11 +1,16 @@
 package com.lkw1120.news.ui
 
 import android.os.Bundle
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.lkw1120.news.R
 import com.lkw1120.news.databinding.ActivityNewsBinding
+import com.lkw1120.news.ui.adapter.WordItemDecoration
+import com.lkw1120.news.ui.adapter.WordListAdapter
 import com.lkw1120.news.viewmodel.NewsViewModel
+import kotlinx.android.synthetic.main.item_news.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
 
@@ -26,10 +31,28 @@ class NewsActivity : AppCompatActivity() {
         binding.apply {
             model = viewModel
             lifecycleOwner = this@NewsActivity
-            //webView.settings.cacheMode = WebSettings.LOAD_NO_CACHE
-            //webView.settings.javaScriptEnabled = true
-            //webView.settings.loadWithOverviewMode = true
-            //webView.loadUrl(viewModel.url.value)
+            newsWordList.addItemDecoration(
+                WordItemDecoration(
+                    root.resources.getDimensionPixelSize(R.dimen.item_word_list_margin_width),
+                    root.resources.getDimensionPixelSize(R.dimen.item_word_list_margin_height)
+                )
+            )
+            val wordListAdapter = WordListAdapter()
+            newsWordList.adapter = wordListAdapter
+            subscribeUi(wordListAdapter)
+
+            newsWebView.webViewClient = WebViewClient()
+            newsWebView.settings.javaScriptEnabled = true
+
         }
+    }
+
+    private fun subscribeUi(adapter: WordListAdapter) {
+        viewModel.news.observe(this, Observer {
+            adapter.submitList(it.words)
+        })
+        viewModel.news.observe(this, Observer {
+            binding.newsWebView.loadUrl(it.link)
+        })
     }
 }
