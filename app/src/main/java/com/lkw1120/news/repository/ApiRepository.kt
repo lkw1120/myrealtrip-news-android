7a35462ee7fd0a5b2a9f2e3ac4f44e26faa9fff1
lkw1120 <lkwkang@gmail.com>
2020-04-03 01:09:48 +0900

    Refactor : 현재 설정된 국가 코드를 파라미터로 사용하도록 변경

--- app/src/main/java/com/lkw1120/news/repository/ApiRepository.kt
+++ app/src/main/java/com/lkw1120/news/repository/ApiRepository.kt
@@ -1,6 +1,7 @@
 package com.lkw1120.news.repository
 
 import android.content.Context
+import android.os.Build
 import com.lkw1120.news.datasource.entity.News
 import com.lkw1120.news.datasource.remote.ApiService
 import com.lkw1120.news.utils.ParserUtil
@@ -10,7 +11,13 @@ class ApiRepository(
     private val apiService: ApiService,
     private val context: Context
 ) {
+    private val locale = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+        context.resources.configuration.locales.get(0)
+    }
+    else {
+        context.resources.configuration.locale
+    }
 
     suspend fun getLatest(): Flow<News> =
-        ParserUtil().parsingRss(apiService.getLatest("ko","kR","KR:ko"))
+        ParserUtil().parsingRss(apiService.getLatest(locale.language,locale.country,"${locale.country}:${locale.language}"))
 }
